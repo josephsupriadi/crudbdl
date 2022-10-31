21f9976835b8395c320905a0dfa9eb33ed4a94b6
josephsupiradi <josephsupiradi86@gmail.com>
2022-11-01 06:53:53 +0700

    edit button dan delete button

--- index.php
+++ index.php
@@ -59,46 +59,89 @@
 
 
 
-    <!-- Function Button Edit -->
-    <div class="modal fade" id="studentaddmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <!-- EDIT POP UP FORM (Bootstrap MODAL) -->
+    <div class="modal fade" id="editmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Add Student Data</h5>
+                    <h5 class="modal-title" id="exampleModalLabel"> Edit Student Data </h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <form action="insert.php" method="POST">
+
+                <form action="updatecode.php" method="POST">
+
                     <div class="modal-body">
 
+                        <input type="hidden" name="update_id" id="update_id">
+
                         <div class="form-group">
-                            <label>First Name</label>
-                            <input type="text" name="fname" class="form-control" placeholder="Enter First Name">
-                        </div>
-                        <div class="form-group">
-                            <label>Last Name</label>
-                            <input type="text" name="lname" class="form-control" placeholder="Enter Last Name">
-                        </div>
-                        <div class="form-group">
-                            <label>Course</label>
-                            <input type="text" name="course" class="form-control" placeholder="Enter Course">
-                        </div>
-                        <div class="form-group">
-                            <label>Phone Number</label>
-                            <input type="number" name="contact" class="form-control" placeholder="Enter Phone Number">
+                            <label> First Name </label>
+                            <input type="text" name="fname" id="fname" class="form-control" placeholder="Enter First Name">
                         </div>
 
+                        <div class="form-group">
+                            <label> Last Name </label>
+                            <input type="text" name="lname" id="lname" class="form-control" placeholder="Enter Last Name">
+                        </div>
 
+                        <div class="form-group">
+                            <label> Course </label>
+                            <input type="text" name="course" id="course" class="form-control" placeholder="Enter Course">
+                        </div>
+
+                        <div class="form-group">
+                            <label> Phone Number </label>
+                            <input type="text" name="contact" id="contact" class="form-control" placeholder="Enter Phone Number">
+                        </div>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="submit" name="insertdata" class="btn btn-primary">Save Data</button>
+                        <button type="submit" name="updatedata" class="btn btn-primary">Update Data</button>
                     </div>
                 </form>
+
             </div>
         </div>
     </div>
+
+
+
+
+    <!-- DELETE POP UP FORM (Bootstrap MODAL) -->
+    <div class="modal fade" id="deletemodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel"> Delete Student Data </h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <form action="deletecode.php" method="POST">
+
+                    <div class="modal-body">
+
+                        <input type="hidden" name="delete_id" id="delete_id">
+
+                        <h4> Apakah Anda Mau menghapus Data Ini ??</h4>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal"> Tidak </button>
+                        <button type="submit" name="deletedata" class="btn btn-primary"> Yaa !! hapus it. </button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+
+
+
+
+
 
     <!-- Heading -->
     <div class="container">
@@ -158,8 +201,12 @@
                                         <td><?php echo $row['course']; ?></td>
                                         <td><?php echo $row['contact']; ?></td>
                                         <td>
-                                            <button type="button" class="btn btn-succes editbtn"> Edit
+                                            <button type="button" class="btn btn-success editbtn"> Edit
                                             </button>
+                                        </td>
+
+                                        <td>
+                                            <button type="button" class="btn btn-danger deletebtn"> DELETE </button>
                                         </td>
                                     </tr>
 
@@ -178,23 +225,63 @@
 
 
 
-
-
         </div>
     </div>
 
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+
+    <script src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.18/js/dataTables.bootstrap4.min.js"></script>
+
+
 
     <script>
         $(document).ready(function() {
+
+            $('.deletebtn').on('click', function() {
+
+                $('#deletemodal').modal('show');
+
+                $tr = $(this).closest('tr');
+
+                var data = $tr.children("td").map(function() {
+                    return $(this).text();
+                }).get();
+
+                console.log(data);
+
+                $('#delete_id').val(data[0]);
+
+            });
+        });
+    </script>
+
+
+
+    <script>
+        $(document).ready(function() {
+
             $('.editbtn').on('click', function() {
 
-                $('#editmodel').modal('show');
+                $('#editmodal').modal('show');
 
-            })
+                $tr = $(this).closest('tr');
+
+                var data = $tr.children("td").map(function() {
+                    return $(this).text();
+                }).get();
+
+                console.log(data);
+
+                $('#update_id').val(data[0]);
+                $('#fname').val(data[1]);
+                $('#lname').val(data[2]);
+                $('#course').val(data[3]);
+                $('#contact').val(data[4]);
+            });
         });
     </script>
 
